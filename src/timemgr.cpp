febc43e7328d3575f93fca8fc24675e97110a6bc
UndefinedCpp <39729414+UndefinedCpp@users.noreply.github.com>
2025-02-21 21:59:22 +0800

    fix: frequent timeouts

--- src/timemgr.cpp
+++ src/timemgr.cpp
@@ -8,18 +8,31 @@ void TimeManager::update(uint32_t remainingTime, uint32_t increment) {
 }
 
 uint32_t TimeManager::spareTime(int ply) {
-    if (_remainingTime <= 1000) {
-        float ratio = _remainingTime / 1000.0f;
-        return (_remainingTime + _increment) * ratio * ratio / 4.0;
+    // Low time handling
+    if (_remainingTime <= 500) {
+        return _increment * 2 / 3;
     }
-    if (ply >= 40) {
-        if (_remainingTime <= 100) {
-            return _increment / 2;
-        } else {
-            return std::max(20u, _remainingTime / 4);
-        }
+    // The following formula is searched
+    //         Ax^2     1 - √x
+    //   r% = ------ + --------
+    //         x+B      Cx + D
+    // where
+    //   A = 0.0030980646
+    //   B = 11.289064
+    //   C = 8.76403
+    //   D = -533.8745
+    if (ply <= 55) {
+        constexpr float A = 0.0030980646;
+        constexpr float B = 11.289064;
+        constexpr float C = 8.76403;
+        constexpr float D = -533.8745;
+        float ratio = (A * ply * ply) / (ply + B) +
+                      (1 - std::sqrt(ply)) / (C * ply + D) + 0.01;
+        float t = _remainingTime * ratio;
+        int time = std::clamp((int)t, (int)_increment / 2,
+                              (int)(_remainingTime - _increment / 2));
+        return time;
     } else {
-        return std::max(_increment - 1,
-                        _remainingTime / (40 - ply) + _increment);
+        return _remainingTime / 2;
     }
 }
